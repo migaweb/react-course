f59c12d6d9d9af730ff79f4bb5dc68b1415b0b8d
micke <miga02@gmail.com>
2020-03-09 20:29:30 +0100

    Added indecision app

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,92 @@
+"use strict";
+
+// babel src/app.js --out-file=public/scripts/app.js --presets="env,react" --watch
+// live-server public
+
+// JSX - Javascript XML
+var app = {
+    title: "Indecision app",
+    subtitle: "Put your life in the hands of a computer",
+    options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        render();
+    }
+};
+
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    render();
+};
+
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var selectedOption = app.options[randomNum];
+    console.log(selectedOption);
+};
+
+var appRoot = document.querySelector("#app");
+
+var render = function render() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length > 0 ? "Here are your options" : "No options"
+        ),
+        React.createElement(
+            "button",
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            "What should I do"
+        ),
+        React.createElement(
+            "button",
+            { onClick: onRemoveAll },
+            "Remove all"
+        ),
+        React.createElement(
+            "ol",
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    "li",
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            "form",
+            { onSubmit: onFormSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add option"
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
