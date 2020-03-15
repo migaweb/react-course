c5951faa39e75989b95671fa6fa46e8aec069c92
micke <miga02@gmail.com>
2020-03-15 19:29:18 +0100

    Added routing to expensify

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,39 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import "normalize.css/normalize.css"
 import "./styles/styles.scss"
 
-ReactDOM.render((<p>This is my boilerplate</p>), document.getElementById("app"))
+const ExpenseDashboardPage = () => (
+    <div>This is from my dahsboard component</div>
+)
+
+const AddExpensePage = () => (
+    <div>This is from my add expense component</div>
+)
+
+const EditExpensePage = () => (
+    <div>Edit page</div>
+)
+
+const HelpPage = () => (
+    <div>Help page</div>
+)
+
+const NotFoundPage = () => (
+    <div>404</div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById("app"))
